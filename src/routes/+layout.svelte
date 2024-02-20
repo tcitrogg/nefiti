27104629390d4e2d354d0d348108047599a1e26c
tcitrogg <tcitrogg@gmail.com>
2024-02-20 19:32:00 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,9 @@
 </script>
 
 <Toaster />
-<ProgressBar class="text-main z-[70]" />
+<section class="fixed top-0 left-0 z-[70]">
+  <ProgressBar class="text-main z-[70]" />
+</section>
 
 <section class="w-full max-h-smscreen md:max-h-screen bg-zinc-50 text-zinc-950 dark:bg-zinc-950 dark:text-zinc-100 font-schibsted_grotesk flex md:flex-row flex-col relative overflow-hidden">
 
