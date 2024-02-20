95698449ab6d1b2693bc906fbf7c83c019282430
tcitrogg <tcitrogg@gmail.com>
2024-02-20 20:47:58 +0100

    Switched the eachPage back to universal cause of the reactive variables, and i moved the menu button down also

--- src/routes/m/[id]/c/[[cid]]/+page.svelte
+++ src/routes/m/[id]/c/[[cid]]/+page.svelte
@@ -109,7 +109,7 @@ let thisPage = 0;
     <Drawer.Root>
       <Drawer.Trigger>
         <!-- <Button builders={[builder]} variant="outline">Open Drawer</Button> -->
-        <button class="block lg:hidden px-2 py-0.5 bg-zinc-300/40 hover:bg-zinc-300/70 dark:bg-zinc-900/40 dark:hover:bg-zinc-900/70 text-zinc-100 backdrop-blur- focus:ring-2 focus:ring-main/50 focus:outline-none rounded-full fixed top-1 left-[45%] z-10">
+        <button class="block lg:hidden px-2 py-0.5 bg-zinc-900/40 hover:bg-zinc-900/70 dark:bg-zinc-300/40 dark:hover:bg-zinc-300/70 text-zinc-100 dark:text-zinc-900 backdrop-blur-xs focus:ring-2 focus:ring-main/50 focus:outline-none rounded-full fixed bottom-1 left-[45%] z-10 shadow-mdshadow-zinc-950">
           <i transition:slide class="icon icon-ic_fluent_re_order_dots_horizontal_20_regular flex text-2xl"/>
         </button>
       </Drawer.Trigger>
