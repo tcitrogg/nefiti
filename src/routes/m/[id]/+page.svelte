60b5896aec4a7a8529b66c36a0cb034ef2a72f69
tcitrogg <tcitrogg@gmail.com>
2024-02-21 08:59:27 +0100

    Update +page.svelte

--- src/routes/m/[id]/+page.svelte
+++ src/routes/m/[id]/+page.svelte
@@ -46,7 +46,7 @@
       <section class="w-full flex md:flex-row flex-col md:gap-4 md:pt-8 lg:pt-14">
         <section class="w-full md:w-4/12">
           <section class="w-full h-96">
-            <a href="/" class="py-1 px-1 rounded-full bg-zinc-950 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-950 text-sm font-semibold ring-2 ring-transparent focus:ring-main/50 focus:outline-none flex items-center gap-3">
+            <a href="/" class="py-1 px-1 rounded-full bg-zinc-950 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-950 text-sm font-semibold ring-2 ring-transparent focus:ring-main/50 focus:outline-none flex md:hidden items-center gap-3 fixed top-2 left-2">
               <i class="icon icon-ic_fluent_home_20_regular flex text-2xl"/>
             </a>
             <section transition:slide={{axis: 'y'}} class="skew-b w-full h-full md:rounded-2xl sticky top-0 left-0 bg-zinc-300 dark:bg-zinc-800 overflow-hidden shadow-md">
